903404de214648ca7d8b9b2911ce09fa58656fea
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-11-30 18:42:18 +0900

    Replクラス追加

--- SeaMonkey/Program.cs
+++ SeaMonkey/Program.cs
@@ -6,7 +6,11 @@ namespace SeaMonkey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine($"Hello {Environment.UserName}! This is the Monkey programming language!");
+            Console.WriteLine("Feel free to type in commands");
+
+            Repl.Repl.Start(Console.In, Console.Out);
         }
     }
 }
---
+++ SeaMonkey/Repl/Repl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SeaMonkey.Repl
+{
+    public class Repl
+    {
+        public static void Start(TextReader reader, TextWriter writer)
+        {
+
+        }
+    }
+}
